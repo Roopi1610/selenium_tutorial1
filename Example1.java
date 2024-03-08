e68d0038c62830087dc6a323ec1ac9583f4b6319
Roopi1610 <162543455+Roopi1610@users.noreply.github.com>
2024-03-08 16:35:39 +1100

    Update and rename Example1.py to Example1.java
    I'm updating JAVA code

---
+++ Example1.java
@@ -0,0 +1,19 @@
+public class Example1
+{
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Roopika");  //String
+		System.out.println("java");     //String
+		System.out.println(5);          //int
+		System.out.println("5");        //String
+		System.out.println(5+5);
+		System.out.println("5+5");
+		System.out.println('t');        //character
+	}
+
+}
+
+
+  
